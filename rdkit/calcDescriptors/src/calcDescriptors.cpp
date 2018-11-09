a88fac7885255766a346abcbbc9f1d945535b873
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-11-09 14:27:29 +0800

    Changed the output header of calcDescriptors

--- rdkit/calcDescriptors/src/calcDescriptors.cpp
+++ rdkit/calcDescriptors/src/calcDescriptors.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	SmilesMolSupplier supplier(argv[1], "\t", 1, 0, false); // Default parameters are: const std::string &fileName, const std::string &delimiter=" \t", int smilesColumn=0, int nameColumn=1, bool titleLine=true, bool sanitize=true)
-	cout << "ID	cSMILES	formula	atoms	HBD	HBA	NRB	rings	MW	TPSA	ClogP" << endl;
+	cout << "ID	CanonicalSMILES	Formula	Atoms	HBD	HBA	RotatableBonds	Rings	ExactMW	TPSA	ClogP" << endl;
 	while (!supplier.atEnd()) {
 		// Obtain a pointer to the current molecule with heavy atoms only.
 		const unique_ptr<ROMol> mol_ptr(supplier.next()); // Calling next() may print "ERROR: Could not sanitize molecule on line XXXX" to stderr.
